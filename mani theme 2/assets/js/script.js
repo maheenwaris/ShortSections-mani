d76f97f7d1a137970384bc56d1c091c9afbfe67f
unknown <mahmil219@gmail.com>
2023-11-09 21:44:05 +0500

    issues fixed

--- mani theme 2/assets/js/script.js
+++ mani theme 2/assets/js/script.js
@@ -1,38 +1,43 @@
-
-const lines = [
+// typewritter
+const textArray = [
     "Pen Tester",
     "Social Engineer",
-    "Python Programmer"
+    "Python programmer"
 ];
 
-const typewriter = document.getElementById("typewriter");
-let lineIndex = 0;
+let typeJsText = document.querySelector(".animatedText");
+let stringIndex = 0;
 let charIndex = 0;
 let isTyping = true;
 
-function typeText() {
-    if (lineIndex < lines.length) {
-        if (charIndex < lines[lineIndex].length) {
-            typewriter.textContent += lines[lineIndex][charIndex];
-            charIndex++;
-            setTimeout(typeText, 100); // Adjust the typing speed (delay) here
+function typeJs() {
+    if (stringIndex < textArray.length) {
+        const currentString = textArray[stringIndex];
+
+        if (isTyping) {
+            if (charIndex < currentString.length) {
+                typeJsText.innerHTML += currentString.charAt(charIndex);
+                charIndex++;
+            } else {
+                isTyping = false;
+            }
         } else {
-            isTyping = false;
-            setTimeout(removeText, 1000); // Remove the line after 2 seconds
+            if (charIndex > 0) {
+                typeJsText.innerHTML = currentString.substring(0, charIndex - 1);
+                charIndex--;
+            } else {
+                isTyping = true;
+                stringIndex++;
+
+                if (stringIndex >= textArray.length) {
+                    stringIndex = 0;
+                }
+
+                charIndex = 0;
+                typeJsText.innerHTML = "";
+            }
         }
     }
 }
 
-function removeText() {
-    if (typewriter.textContent.length > 0) {
-        typewriter.textContent = typewriter.textContent.slice(0, -1);
-        setTimeout(removeText, 50); // Adjust the erasing speed (delay) here
-    } else {
-        isTyping = true;
-        lineIndex++;
-        if (lineIndex < lines.length) {
-            setTimeout(typeText, 1000); // Start typing the next line after a 1-second delay
-        }
-    }
-}
-typeText(); // Start typing the first line
+setInterval(typeJs, 100);
